0b2f5eadeb241a20aaa03ee7267772817b9dadcf
Paul-Michael Agapow <pma+github@agapow.net>
2012-02-29 16:24:11 +0000

    added assay types to sequence

--- app/viewhints/susceptibility_sequence_hints.rb
+++ app/viewhints/susceptibility_sequence_hints.rb
@@ -4,11 +4,15 @@ class SusceptibilitySequenceHints < Hobo::ViewHints
 	model_name_plural "Genotypic analyses"
 	
 	field_names({
-		:title => "Title or ID",
-		})
+		:title        => "Title or ID",
+		:assay        => "Assay type",
+		:assay_other  => "Other assay type",
+	})
 
 	field_help({
-		:title => "Unique or reference identifier (optional)",
+		:title => "Unique or reference identifier for the sequence (optional)",
+		:assay => "How was the sequence derived?",
+		:assay_other => "If not one of the above assay types, fill this in.",
 	})
 
 end
